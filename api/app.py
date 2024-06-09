828da1a235f2321885000d39c73d6291fb3f3f0a
Cristian F <158538142+CristianFC23@users.noreply.github.com>
2024-06-08 22:36:43 -0500

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,57 +1,112 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
-from model.company import (
-    get_companies,
-    get_company,
-    create_company,
-    get_company_by_name,
-    update_company,
-    delete_company,
+from model.Ubicaciones import (
+    get_ubicaciones,
+    get_ubi_id,
+    get_sala,
+    create_sala,
+    update_sala,
+    delete_sala
+)
+
+from model.Equipo import (
+    get_equipos,
+    get_equipo_id,
+    get_equipo_marca,
+    create_equipo,
+    update_equipo,
+    delete_equipo
 )
 
 from model.users import (
     get_users,
-    get_user,
+    get_user_id,
     create_user,
     update_user,
     delete_user,
 )
 
-app = Flask(__name__)
+app = Flask(_name_)
 CORS(app)
 
-@app.route('/companies', methods=['GET'])
-def list_companies():
-    retorno = get_companies()
+#Ubicacion
+@app.route('/ubicacion', methods=['GET'])
+def list_ubicacion():
+    retorno = get_ubicaciones()
     return jsonify(retorno)
 
-@app.route('/companies/<int:company_id>', methods=['GET']) # mapeo automatico de enteros con etiqueta company_id
-def get_company_by_id(company_id):
-    return jsonify(get_company(company_id))
+@app.route('/ubicacion/<int:ubi_id>', methods=['GET']) 
+def get_ubicacion_by_id(ubi_id):
+    return jsonify(get_ubi_id(ubi_id))
 
-@app.route('/companies/by_name/<company_name>', methods=['GET'])
-def get_company_by_name(company_name):
-    return jsonify(get_company_by_name(company_name)) # las funciones deben tener diferentes nombres
+@app.route('/ubicacion/sala/<ubicacion_name>', methods=['GET'])
+def get_ubi_by_name(ubicacion_name):
+    return jsonify(get_sala(ubicacion_name)) 
 
-@app.route('/companies', methods=['POST'])
-def create_companies():
+@app.route('/ubicacion', methods=['POST'])
+def create_ubi():
     data = request.get_json()
     name = data['name']
     description = data['description']
-    return jsonify(create_company(name, description))
+    paciente_1= data['paciente']
+    equipo_1= data['equipos']
+    return jsonify(create_sala(name, description,paciente_1,equipo_1))
 
-@app.route('/companies/<int:company_id>', methods=['PUT']) # methods=['PUT', 'PATCH']
-def update_companies(company_id):
+@app.route('/ubicacion/<int:ubi_id>', methods=['PUT']) 
+def update_ubi(ubi_id):
     data = request.get_json()
-    name = data['name']
-    description = data['description']
-    return jsonify(update_company(name, description, company_id))
+    name = data['salas']
+    description = data['descripcion']
+    paciente_1= data['paciente']
+    equipo_1= data['equipos']
+    return jsonify(update_sala(name, description,paciente_1,equipo_1, ubi_id))
 
-@app.route('/companies/<int:company_id>', methods=['DELETE'])
-def delete_companies(company_id):
-    return jsonify(delete_company(company_id))
+@app.route('/ubicacion/<int:ubi_id>', methods=['DELETE'])
+def delete_companies(ubi_id):
+    return jsonify(delete_sala(ubi_id))
 
+
+#Equipos
+@app.route('/equipo', methods=['GET'])
+def list_equipo():
+    retorno = get_equipos()
+    return jsonify(retorno)
+
+@app.route('/equipo/<int:equipo_id>', methods=['GET']) 
+def get_equipo_by_id(equipo_id):
+    return jsonify(get_equipo_id(equipo_id))
+
+@app.route('/equipo/nombre/<equipo_name>', methods=['GET'])
+def get_equipo_by_name(equipo_name):
+    return jsonify(get_equipo_marca(equipo_name)) 
+
+@app.route('/equipo', methods=['POST'])
+def create_equipo():
+    data = request.get_json()
+    name = data['nombre']
+    marca_1 = data['marca']
+    description = data['descripcion']
+    mant= data['mantenimiento']
+    fecha= data['fecha_mant']
+    return jsonify(create_equipo(name,marca_1,description,mant,fecha))
+
+@app.route('/equipo/<int:equipo_id>', methods=['PUT']) 
+def update_equipo(equipo_id):
+    data = request.get_json()
+    name = data['nombre']
+    marca_1 = data['marca']
+    description = data['descripcion']
+    mant= data['mantenimiento']
+    fecha= data['fecha_mant']
+    return jsonify(update_equipo(name,marca_1,description,mant,fecha, equipo_id))
+
+@app.route('/equipo/<int:equipo_id>', methods=['DELETE'])
+def delete_equipo(equipo_id):
+    return jsonify(delete_equipo(equipo_id))
+
+
+#users
 @app.route('/users', methods=['GET'])
 def list_users():
     retorno = get_users()
@@ -59,21 +114,29 @@ def list_users():
 
 @app.route('/users/<int:user_id>', methods=['GET'])
 def get_users(user_id):
-    return jsonify(get_user(user_id))
+    return jsonify(get_user_id(user_id))
 
 @app.route('/users', methods=['POST'])
 def create_users():
     data = request.get_json()
-    name = data['name']
-    lastname = data['lastname']
-    return jsonify(create_user(name, lastname))
+    name = data['nombre']
+    lastname = data['apellido']
+    sint = data['sintomas']
+    ubi = data['ubicacion']
+    medic = data['medico']
+    equip = data['equipo']
+    return jsonify(create_user(name, lastname, sint, ubi, medic, equip))
 
 @app.route('/users/<int:user_id>', methods=['PUT'])
 def update_users(user_id):
     data = request.get_json()
-    name = data['name']
-    lastname = data['lastname']
-    return jsonify(update_user(name, lastname, user_id))
+    name = data['nombre']
+    lastname = data['apellido']
+    sint = data['sintomas']
+    ubi = data['ubicacion']
+    medic = data['medico']
+    equip = data['equipo']
+    return jsonify(update_user(name, lastname, sint, ubi, medic, equip, user_id))
 
 @app.route('/users/<int:user_id>', methods=['DELETE'])
 def delete_users(user_id):
